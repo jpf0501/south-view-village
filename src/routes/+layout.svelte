1da23064b7f576dda4e64035c23c67fa107ab332
Rodel Gerodias <rodelgerodias25@gmail.com>
2022-11-28 18:22:10 +0800

    restructured the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,10 @@
 <script>
 	import '../app.postcss';
-	import Footer from './Footer.svelte';
-	import Header from './Header.svelte';
-	import Nav from './Nav.svelte';
+	
 </script>
 
 <svelte:head>
 	<title>Southview Village</title>
 </svelte:head>
 
-<main class="flex flex-col min-h-full">
-	<Header />
-	<Nav />
-	<slot />
-	<Footer />
-</main>
+<slot/>
