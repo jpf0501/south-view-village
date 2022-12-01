2695881a939f359070539d652c7827e1ade15bfb
justinefer <justinefer@airmail.cc>
2022-12-01 23:20:45 +0800

    update dashboard

--- src/routes/(admin-side)/admin/+page.svelte
+++ src/routes/(admin-side)/admin/+page.svelte
@@ -1,1 +1,26 @@
-Dashboard
+<div class="min-w-full min-h-full bg-base-200">
+    <h1 class="text-3xl p-12">Dashboard</h1>
+	<div class="flex flex-col md:flex-row flex-wrap pt-4">
+		<div class="basis-1/3 flex flex-col items-end">
+            <div class="w-60 h-40 overflow-hidden shadow-lg border rounded-xl bg-base-100">
+                <p>Card 1</p>
+            </div>
+            <div class="w-60 h-40 overflow-hidden shadow-lg border rounded-xl bg-base-100">
+                <p>Card 2</p>
+            </div>
+            <div class="w-60 h-40 overflow-hidden shadow-lg border rounded-xl bg-base-100">
+                <p>Card 3</p>
+            </div>
+        </div>
+        <div class="basis-2/3 flex justify-center">
+            <div class="w-full max-w-3xl shadow-2xl border rounded-xl bg-base-100">
+                <div class="p-4">
+                    <h1 class="text-2xl mb-8">Upcoming Events</h1>
+                    <p>Data 1</p>
+                    <p>Data 2</p>
+                    <p>Data 3</p>
+                </div>
+            </div>
+	    </div>
+    </div>
+</div>
