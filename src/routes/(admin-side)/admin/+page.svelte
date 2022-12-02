ed6e84ba9c052d36ab1df23ae2e622943e53a86a
justinefer <justinefer@airmail.cc>
2022-12-02 11:31:24 +0800

    add extra formatting to dashboard

--- src/routes/(admin-side)/admin/+page.svelte
+++ src/routes/(admin-side)/admin/+page.svelte
@@ -1,24 +1,38 @@
 <div class="min-w-full min-h-full bg-base-200">
-    <h1 class="text-3xl p-12">Dashboard</h1>
-	<div class="flex flex-col md:flex-row flex-wrap pt-4">
-		<div class="basis-1/3 flex flex-col items-end">
-            <div class="w-60 h-40 overflow-hidden shadow-lg border rounded-xl bg-base-100">
-                <p>Card 1</p>
+    <h1 class="text-3xl font-semibold p-12">Dashboard</h1>
+	<div class="flex flex-col md:flex-row pt-4">
+		<div class="basis-1/3 flex flex-col items-center lg:items-end space-y-4 p-5">
+            <div class="w-60 h-40 flex flex-col justify-center items-center overflow-hidden shadow-lg border rounded-xl bg-base-100">
+                <p class="text-3xl font-semibold">43</p>
+                <p class="text-md mt-2">Pending Dues</p>
             </div>
-            <div class="w-60 h-40 overflow-hidden shadow-lg border rounded-xl bg-base-100">
-                <p>Card 2</p>
+            <div class="w-60 h-40 flex flex-col justify-center items-center overflow-hidden shadow-lg border rounded-xl bg-base-100">
+                <p class="text-3xl font-semibold">2</p>
+                <p class="text-md mt-2">Booking Requests</p>
             </div>
-            <div class="w-60 h-40 overflow-hidden shadow-lg border rounded-xl bg-base-100">
-                <p>Card 3</p>
+            <div class="w-60 h-40 flex flex-col justify-center items-center overflow-hidden shadow-lg border rounded-xl bg-base-100">
+                <p class="text-3xl font-semibold">3</p>
+                <p class="text-md mt-2">Accounts</p>
             </div>
         </div>
-        <div class="basis-2/3 flex justify-center">
+        <div class="basis-2/3 flex justify-center p-5">
             <div class="w-full max-w-3xl shadow-2xl border rounded-xl bg-base-100">
                 <div class="p-4">
-                    <h1 class="text-2xl mb-8">Upcoming Events</h1>
-                    <p>Data 1</p>
-                    <p>Data 2</p>
-                    <p>Data 3</p>
+                    <h1 class="text-2xl mb-8 font-semibold p-3">Upcoming Events</h1>
+                    <div class="flex flex-col space-y-8 text-md p-3">
+                        <div>
+                            <p class="font-medium text-md mb-3">Christmas Party</p>
+                            <p>Dec 18 3:00 PM </p>
+                        </div>
+                        <div>
+                            <p class="font-medium text-md mb-3">New Years Gathering</p>
+                            <p>Dec 31 10:00 PM </p>
+                        </div>
+                        <div>
+                            <p class="font-medium text-md mb-3">Meeting</p>
+                            <p>Jan 3 8:00 PM </p>
+                        </div>
+                    </div> 
                 </div>
             </div>
 	    </div>
