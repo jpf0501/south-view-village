b08bb824c96093a62216c6dd76b3e33f2e9f498e
Rodel Gerodias <rodelgerodias25@gmail.com>
2022-12-09 18:22:31 +0800

    improve design account page 2

--- src/routes/(admin-side)/admin/accounts/+page.svelte
+++ src/routes/(admin-side)/admin/accounts/+page.svelte
@@ -13,31 +13,29 @@
 </script>
 
 <main>
-	<div class="my-5 mx-12">
-		<div>
-			<h1 class="text-3xl font-bold my-4">Accounts</h1>
-		</div>
-		<div class="flex justify-between m-3">
-			<h1 class="px-1 text-xl font-semibold">Users</h1>
+	<div class="min-w-full min-h-full bg-base-200">
+		<h1 class="text-3xl font-semibold p-12">Accounts</h1>
+		<div class="flex justify-between px-10">
+			<h1 class="text-2xl font-semibold">Users</h1>
 			<input type="search" placeholder="Search here" />
 			<a
 				class="px-1 text-base bg-blue-400 rounded-full hover:bg-blue-800 flex items-center border-blue-700"
 				href="/admin/accounts/create">Add User</a
 			>
 		</div>
-		<div>
+		<div class="px-10 py-5">
 			<table class="border-2 border-black w-full">
 				<thead>
-					<tr class=" px-5 font-bold bg-gray-500">
+					<tr class="font-bold bg-gray-500">
 						<th class="text-left text-base">Name</th>
-						<th class="text-left text-base">Email</th>
 						<th class="text-left text-base">Address</th>
+						<th class="text-left text-base">Email</th>
 						<th class="text-left text-base">Role</th>
 					</tr>
 				</thead>
 				<tbody>
 					{#each listOfUsers as user}
-						<tr class="text-base text-center">
+						<tr class="text-base text-center border-t-2 border-black">
 							<td class="text-left text-base bg-gray-300">{user.firstname + ' ' + user.lastname}</td
 							>
 							<td class="text-left text-base bg-gray-300"
--- src/routes/(admin-side)/admin/news/+page.svelte
+++ src/routes/(admin-side)/admin/news/+page.svelte
@@ -1,51 +1,47 @@
 <main>
-    <div>
-        <div>
-            <h1 class="text-3xl font-bold mt-5 mb-16 ml-5">News</h1>
-        </div>
-        <div>
-            <div class="mb-5">
-                <div class="flex justify-between border-2 border-black text-2xl">
-                    <h1 class="">Title</h1>
-                    <p>Description</p>
-                    <h1 class="">Date</h1>
-                    <a class="px-1 text-sm bg-blue-400 rounded-full hover:bg-blue-800 flex items-center border-blue-700" href="/admin/news/create">Add News</a>
-                </div>
-            </div>
-            <div>
-                <ul>
-                    <li class="border-2 border-black pb-5">
-                        <div class="flex justify-between">
-                            <span>Tiltle</span>
-                            <span>Description</span>
-                            <span>Date</span>
-                            <button>Image</button>
-                            <button>Edit</button>
-                            <button>Remove</button>
-                        </div>                             
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <div class="flex justify-center mt-5">
-                    <div>
-                        <div class="bg-blue-500">
-                            <ul>
-                                <a href="/admin/news" class="border border-black">1</a>
-                                <a href="/admin/news" class="border border-black">2</a>
-                                <a href="/admin/news" class="border border-black">3</a>
-                                <a href="/admin/news" class="border border-black">4</a>
-                                <a href="/admin/news" class="border border-black">5</a>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div> 
-        </div>
-    </div>
+	<div>
+		<div>
+			<h1 class="text-3xl font-bold mt-5 mb-16 ml-5">News</h1>
+		</div>
+		<div>
+			<div class="mb-5">
+				<div class="flex justify-between border-2 border-black text-2xl">
+					<h1 class="">Title</h1>
+					<p>Description</p>
+					<h1 class="">Date</h1>
+					<a
+						class="px-1 text-sm bg-blue-400 rounded-full hover:bg-blue-800 flex items-center border-blue-700"
+						href="/admin/news/create">Add News</a
+					>
+				</div>
+			</div>
+			<div>
+				<ul>
+					<li class="border-2 border-black pb-5">
+						<div class="flex justify-between">
+							<span>Tiltle</span>
+							<span>Description</span>
+							<span>Date</span>
+							<button>Edit</button>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div>
+				<div class="flex justify-center mt-5">
+					<div>
+						<div class="bg-blue-500">
+							<ul>
+								<a href="/admin/news" class="border border-black">1</a>
+								<a href="/admin/news" class="border border-black">2</a>
+								<a href="/admin/news" class="border border-black">3</a>
+								<a href="/admin/news" class="border border-black">4</a>
+								<a href="/admin/news" class="border border-black">5</a>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>
-
-
-
-
-
