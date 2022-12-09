749aa2a46424ba2905bc463d88f8eb5f4a0fde8d
Emetsu <valmores.russel@gmail.com>
2022-12-09 14:50:47 +0800

    implement account display

--- src/routes/(admin-side)/admin/accounts/+page.svelte
+++ src/routes/(admin-side)/admin/accounts/+page.svelte
@@ -1,23 +1,16 @@
 <script>
+    import { onSnapshot, query, collection, snapshotEqual } from "firebase/firestore";
+    import { db } from "$lib/firebase/client";
+	import { onDestroy } from "svelte";
 
-    let user = {
-        name: 'Michael Angelo',
-        address: '12 Blk 2 Lot 3',
-        email: 'michael@yahoo.com',
-        role: 'Admin',
-        status: 'Paid'
-    };
-    let listOfUsers = [
-        {
-            user
-        },
-        {
-            user
-        },{
-            user
-        },
-        
-    ];
+    let listOfUsers = [];
+
+    const accountsQuery = query(collection(db, "accounts"))
+    const unsubscribe = onSnapshot(accountsQuery, (querySnapshot) => {
+        listOfUsers = querySnapshot.docs.map((doc) => ({ id:doc.id, ...doc.data() }))
+    })
+    onDestroy(() => unsubscribe())
+
 </script>
 
 
@@ -39,18 +32,26 @@
                         <th class="py-2 px-32 text-left bg-orange-200">Address</th>
                         <th class="py-2 px-28 text-left bg-yellow-200">Email</th>
                         <th class="py-2 px-10 text-left bg-lime-200">Role</th>
-                        <th class="py-2 px-10 text-left bg-cyan-200">Status</th>
                     </tr>
                 </thead>
                <tbody>  
-                    {#each listOfUsers as item}
+                    <!-- {#each listOfUsers as item}
                     <tr class="text-base" value={item.user}>
                         <td class="py-2 pr-28 text-left bg-blue-200" >{user.name}</td>
                         <td class="py-2 px-32 text-left bg-red-200">{user.address}</td>
                         <td class="py-2 px-28 text-left bg-green-200">{user.email}</td>
                         <td class="py-2 px-10 text-center">{user.role}</td>
-                        <td class="py-2 px-10 text-center text-green-400">{user.status}</td>
                         <a class="py-2 px-10" href="/admin/accounts">Edit</a>
+                    </tr>
+                    {/each} -->
+                    {#each listOfUsers as user}
+                    <tr class="text-base">
+                        <td class="py-2 pr-28 text-left bg-blue-200" >{user.firstname}</td>
+                        <td class="py-2 px-32 text-left bg-red-200">{user.addressBlock}{user.addressLot}{user.addressStreet}</td>
+                        <td class="py-2 px-28 text-left bg-green-200">{user.contactNumber}</td>
+                        <td class="py-2 px-10 text-center">{user.role}</td>
+                        <a class="py-2 px-10" href="/admin/accounts">Edit</a>
+                        <!-- <h1>{ JSON.stringify(user) }</h1> -->
                     </tr>
                     {/each}
                </tbody>
