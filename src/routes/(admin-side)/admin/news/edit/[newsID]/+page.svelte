9fbd48921e69d1dfa62a506abc7eb8507de6ab47
Rodel Gerodias <rodelgerodias25@gmail.com>
2022-12-22 13:57:38 +0800

    added calendar page in admin side

--- src/routes/(admin-side)/admin/news/edit/[newsID]/+page.svelte
+++ src/routes/(admin-side)/admin/news/edit/[newsID]/+page.svelte
@@ -9,13 +9,13 @@
 
 	let news = null;
 
-    async function getNews() {
-        const snapshot = await getDoc(doc(db, 'news', newsID));
-        news = snapshot.data();
-    }
-    getNews();
+	async function getNews() {
+		const snapshot = await getDoc(doc(db, 'news', newsID));
+		news = snapshot.data();
+	}
+	getNews();
 
-    async function updateNews() {
+	async function updateNews() {
 		// console.log(user);
 		try {
 			await updateDoc(doc(db, 'news', newsID), news);
@@ -27,60 +27,63 @@
 		}
 	}
 
-    async function deleteNews() {
-        try {
-            await deleteDoc(doc(db, 'news', newsID), news);
-            alert('Entry deleted');
-            await goto('/admin/news');
-        } catch (error) {
-            console.log(error);
-            alert('Error deleting');
-        }
-    }
-
+	async function deleteNews() {
+		try {
+			await deleteDoc(doc(db, 'news', newsID), news);
+			alert('Entry deleted');
+			await goto('/admin/news');
+		} catch (error) {
+			console.log(error);
+			alert('Error deleting');
+		}
+	}
 </script>
 
 {#if news}
-    <div>
-        <div class="grid place-items-center py-10">
-            <div class="w-11/12">
-                <div>
-                    <h1 class="text-xl font-bold bg-gray-300 px-1 py-3">Edit News</h1>              
-                </div>
-                <form action="">
-                    <div class="">
-                        <label for="title">Title</label>
-                        <br>
-                        <input type="text" class="w-1/2" required bind:value={news.title}> 
-                    </div>   
-                    <div >
-                        <label for="content">Content</label>
-                        <br>
-                        <textarea class="h-60 w-5/6" required bind:value={news.content}></textarea>
-                    </div>            
-                    <div >
-                        <label for="date">Date</label>
-                        <br>
-                        <!-- Temporary -->
-                        <input type="text" class="w-1/2" required bind:value={news.date}>
-                        <!-- Temporary -->
-                    </div> 
-                    <div >
-                        <label for="time">Time</label>
-                        <br>
-                        <!-- Temporary -->
-                        <input type="text" class="w-1/2" required bind:value={news.time}>
-                        <!-- Temporary -->
-                    </div> 
-                    <div class="grid place-items-center my-10">
-                        <div class="flex gap-5">
-                            <button on:click={updateNews} type="submit" class="bg-blue-500 px-10 py-2">Submit</button>
-                            <button on:click={deleteNews} type="submit" class="bg-red-500 px-10 py-2">Delete</button>
-                            <a href="/admin/news" class="bg-green-500 px-10 py-2">Cancel</a>
-                        </div>          
-                    </div>
-                </form>     
-            </div>         
-        </div>
-    </div> 
+	<div>
+		<div class="grid place-items-center py-10">
+			<div class="w-11/12">
+				<div>
+					<h1 class="text-xl font-bold bg-gray-300 px-1 py-3">Edit News</h1>
+				</div>
+				<form action="">
+					<div class="">
+						<label for="title">Title</label>
+						<br />
+						<input type="text" class="w-1/2" required bind:value={news.title} />
+					</div>
+					<div>
+						<label for="content">Content</label>
+						<br />
+						<textarea class="h-60 w-5/6" required bind:value={news.content} />
+					</div>
+					<div>
+						<label for="date">Date</label>
+						<br />
+						<!-- Temporary -->
+						<input type="text" class="w-1/2" required bind:value={news.date} />
+						<!-- Temporary -->
+					</div>
+					<div>
+						<label for="time">Time</label>
+						<br />
+						<!-- Temporary -->
+						<input type="text" class="w-1/2" required bind:value={news.time} />
+						<!-- Temporary -->
+					</div>
+					<div class="grid place-items-center my-10">
+						<div class="flex gap-5">
+							<button on:click={updateNews} type="submit" class="bg-blue-500 px-10 py-2"
+								>Confirm Edit</button
+							>
+							<button on:click={deleteNews} type="submit" class="bg-red-500 px-10 py-2"
+								>Delete</button
+							>
+							<a href="/admin/news" class="bg-green-500 px-10 py-2">Cancel</a>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 {/if}
