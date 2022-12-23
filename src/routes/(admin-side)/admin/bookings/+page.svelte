508ebb273efe12cee2a9b4933cf9bea0cbae4d2c
justinefer <justinefer@airmail.cc>
2022-12-24 00:25:52 +0800

    update admin booking page

--- src/routes/(admin-side)/admin/bookings/+page.svelte
+++ src/routes/(admin-side)/admin/bookings/+page.svelte
@@ -25,16 +25,20 @@
 							<th class="text-left text-base">Name</th>
 							<th class="text-left text-base">Email Address</th>
 							<th class="text-left text-base">Contact No.</th>
-                            <th class="text-left text-base">Status</th>
+                            <th class="text-left text-base">Type of Event</th>
+							<th class="text-left text-base">Date</th>
+							<th class="text-left text-base">Time</th>
 						</tr>
 					</thead>
 					<tbody>
 						{#each listOfBooking as book}
 							<tr class="text-base text-center border-t-2 border-black">
-								<td class="text-left text-base bg-gray-300">{book.name}</td>
+								<td class="text-left text-base bg-gray-300">{book.firstName} {book.lastName}</td>
 								<td class="text-left text-base bg-gray-300">{book.email}</td>
-								<td class="text-left text-base bg-gray-300">{book.contact}</td>
-                                <td class="text-left text-base bg-gray-300">{book.status}</td>
+								<td class="text-left text-base bg-gray-300">{book.contactNumber}</td>
+                                <td class="text-left text-base bg-gray-300">{book.eventType}</td>
+								<td class="text-left text-base bg-gray-300">{book.date}</td>
+								<td class="text-left text-base bg-gray-300">{book.time}</td>
                                 <button class="py-2 px-10 bg-green-500">Approve</button>
                                 <button class="py-2 px-10 bg-red-500">Dissaprove</button>
 							</tr>
