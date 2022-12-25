592c54a138717a0f07e1acb2f97147e39c813216
Rodel Gerodias <rodelgerodias25@gmail.com>
2022-12-25 13:53:29 +0800

    improve design and responsivness in table accounts

--- src/routes/(admin-side)/admin/accounts/+page.svelte
+++ src/routes/(admin-side)/admin/accounts/+page.svelte
@@ -51,7 +51,7 @@
 <div class="min-w-full min-h-full bg-base-200 px-12">
 	<h1 class="text-3xl font-semibold py-12">Accounts</h1>
 	<div class="flex justify-between">
-		<h1 class="text-2xl font-semibold">Users</h1>
+		<h1 class="text-xl font-semibold">Users</h1>
 		<input type="search" placeholder="Search here" />
 		<form on:submit|preventDefault={sortBy}>
 			<select bind:value={userSort} on:click={sortBy} name="" id="">
@@ -64,25 +64,28 @@
 			</select>
 		</form>
 		<a
-			class="px-1 text-base bg-gray-400 rounded-full hover:bg-gray-300 flex items-center border-gray-700"
+			class="px-1 text-sm bg-gray-400 rounded-full hover:bg-gray-300 flex items-center border-gray-700"
 			href="/admin/accounts/create">Add User</a
 		>
 	</div>
-	<div class="py-5 m-auto">
+
+	<!-- Medium to large screen -->
+	<div class="my-5 p-5 overflow-auto shadow-lg border rounded-xl bg-gray-300 hidden md:block">
 		<table class="border-2 border-black bg-white w-full">
-			<thead>
-				<tr class="font-bold bg-gray-500">
-					<th class="text-left">Name</th>
-					<th class="text-left">Address</th>
-					<th class="text-left">Email</th>
-					<th class="text-left">Role</th>
+			<thead class="font-bold bg-gray-500">
+				<tr>
+					<th class="p-3 text-sm tracking-wide text-left">Name</th>
+					<th class="p-3 text-sm tracking-wide text-left">Address</th>
+					<th class="p-3 text-sm tracking-wide text-left">Email</th>
+					<th class="p-3 text-sm tracking-wide text-left">Role</th>
+					<th class="p-3 text-sm tracking-wide text-left" />
 				</tr>
 			</thead>
 			<tbody>
 				{#each listOfUsers as user}
 					<tr class="border-t-2 border-black">
-						<td class="text-left ">{user.firstname + ' ' + user.lastname}</td>
-						<td class="text-left "
+						<td class="p-3 text-sm whitespace-nowrap">{user.firstname + ' ' + user.lastname}</td>
+						<td class="p-3 text-sm whitespace-nowrap"
 							>{'Block ' +
 								user.addressBlock +
 								' Lot ' +
@@ -91,13 +94,56 @@
 								user.addressStreet +
 								' Street'}</td
 						>
-						<td class="text-left">{user.email}</td>
-						<td class="text-left">{user.role}</td>
-
-						<a href={'/admin/accounts/edit/' + user.id} class="text-blue-500">Edit</a>
+						<td class="p-3 text-sm whitespace-nowrap">{user.email}</td>
+						<td class="p-3 text-sm whitespace-nowrap">{user.role}</td>
+						<td class="p-3 text-sm whitespace-nowrap">
+							<a
+								href={'/admin/accounts/edit/' + user.id}
+								class="font-bold text-blue-500 hover:underline">Edit</a
+							>
+						</td>
 					</tr>
 				{/each}
 			</tbody>
 		</table>
+	</div>
+
+	<!-- Small screen -->
+	<div class="bg-gray-300 my-5 p-5  selection:grid grid-cols-1 gap-4 md:hidden rounded-lg shadow">
+		{#each listOfUsers as user}
+			<div class="bg-white space-y-3 p-4 border-2 border-black">
+				<div class="flex items-center space-x-2  text-sm">
+					<div>
+						<span class="font-bold text-sm">Name: </span>
+						{user.firstname + ' ' + user.lastname}
+					</div>
+					<div>
+						<span class="font-bold text-sm">Role: </span>
+						{user.role}
+					</div>
+					<div>
+						<a
+							href={'/admin/accounts/edit/' + user.id}
+							class="text-blue-500 font-bold hover:underline">Edit</a
+						>
+					</div>
+				</div>
+				<div>
+					<span class="font-bold text-sm">Address: </span>
+					{'Block ' +
+						user.addressBlock +
+						' Lot ' +
+						user.addressLot +
+						' ' +
+						user.addressStreet +
+						' Street'}
+				</div>
+				<div>
+					<span class="font-bold text-sm">Email: </span>
+					{user.email}
+				</div>
+			</div>
+			<br />
+		{/each}
 	</div>
 </div>
