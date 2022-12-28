0e18088d8e3b3fdadb5adf48998028631dd6ef0f
Rodel Gerodias <rodelgerodias25@gmail.com>
2022-12-28 20:14:38 +0800

    added booking history page

--- src/routes/(admin-side)/admin/bookings/bookingsHistory/+page.svelte
+++ src/routes/(admin-side)/admin/bookings/bookingsHistory/+page.svelte
@@ -1,8 +1,7 @@
 <script>
-	import { onSnapshot, query, collection, updateDoc, doc, orderBy } from 'firebase/firestore';
+	import { onSnapshot, query, collection, orderBy, where } from 'firebase/firestore';
 	import { db } from '$lib/firebase/client';
 	import { onDestroy } from 'svelte';
-	import { sendEmail } from '$lib/utils';
 
 	let listOfBooking = [];
 
@@ -46,62 +45,48 @@
 					listOfBooking = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 				});
 				onDestroy(() => unsubscribe());
+			} else if (requestSort == 'Approved') {
+				const sortByApprovedQuery = query(
+					collection(db, 'booking'),
+					orderBy('time', 'asc'),
+					where('status', '==', 'Approved')
+				);
+				const unsubscribe = onSnapshot(sortByApprovedQuery, (querySnapshot) => {
+					listOfBooking = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+				});
+				onDestroy(() => unsubscribe());
+			} else if (requestSort == 'Disapproved') {
+				const sortByDisapprovedQuery = query(
+					collection(db, 'booking'),
+					orderBy('time', 'asc'),
+					where('status', '==', 'Disapproved')
+				);
+				const unsubscribe = onSnapshot(sortByDisapprovedQuery, (querySnapshot) => {
+					listOfBooking = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+				});
+				onDestroy(() => unsubscribe());
+			} else if (requestSort == 'Pending') {
+				const sortByPendingQuery = query(
+					collection(db, 'booking'),
+					orderBy('time', 'asc'),
+					where('status', '==', 'Pending')
+				);
+				const unsubscribe = onSnapshot(sortByPendingQuery, (querySnapshot) => {
+					listOfBooking = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+				});
+				onDestroy(() => unsubscribe());
 			}
 		} catch (error) {
 			console.log(error);
 			// alert('Error in sorting');
 		}
 	}
-
-	async function approveBook(bookingId) {
-		try {
-			const bookref = doc(db, 'booking', bookingId);
-			const data = {
-				status: 'Approved'
-			};
-			await updateDoc(bookref, data);
-			alert('Booking request has been approved');
-		} catch (error) {
-			console.log(error);
-			alert('Error approving request');
-		}
-	}
-
-	async function disapproveBook(bookingId) {
-		try {
-			const bookref = doc(db, 'booking', bookingId);
-			const data = {
-				status: 'Disapproved'
-			};
-			await updateDoc(bookref, data);
-			alert('Booking request has been disapproved');
-		} catch (error) {
-			console.log(error);
-			alert('Error disapproving request');
-		}
-	}
-
-	async function sendPaymentEmail(bookEmail) {
-		// console.log(bookEmail);
-		try {
-			const result = await sendEmail({
-				to: bookEmail,
-				subject: 'Southview Homes 3 Payment Method',
-				html: '<h1>This is the link for payment for reservation in booking</ht>'
-			});
-			console.log(JSON.stringify(result));
-			alert('Email for payment method sent successfully');
-		} catch (error) {
-			console.log(error);
-			alert('Error in sending payment method');
-		}
-	}
 </script>
 
 <div class="min-w-full min-h-full bg-base-200 px-12">
 	<div class="flex justify-between">
-		<h1 class="text-3xl font-semibold py-12">Booking</h1>
-		<a href="/admin/bookings/bookingsHistory" class="hover:underline">Go to History</a>
+		<h1 class="text-3xl font-semibold py-12">Booking History</h1>
+		<a href="/admin/bookings" class="hover:underline">Go to Bookings</a>
 	</div>
 	<div class="flex justify-between">
 		<h1 class="text-xl font-semibold">Requests</h1>
@@ -113,10 +98,12 @@
 			<option value="Event">Type of Event</option>
 			<option value="Date">Date</option>
 			<option value="Time">Time</option>
+			<option value="Approved">Approved</option>
+			<option value="Disapproved">Disapproved</option>
+			<option value="Pending">Pending</option>
 		</select>
 	</div>
 
-	<!-- Medium to large screen -->
 	<div class="my-5 p-5 overflow-auto shadow-lg border rounded-xl bg-gray-300 hidden md:block">
 		<table class="border-2 border-black bg-white w-full text-center">
 			<thead class="font-bold bg-gray-500">
@@ -127,7 +114,8 @@
 					<th class="p-3 text-sm tracking-wide">Type of Event</th>
 					<th class="p-3 text-sm tracking-wide">Date</th>
 					<th class="p-3 text-sm tracking-wide">Time</th>
-					<th class="p-3 text-sm tracking-wide" colspan="3" />
+					<th class="p-3 text-sm tracking-wide">Status</th>
+					<th class="p-3 text-sm tracking-wide" />
 				</tr>
 			</thead>
 			<tbody>
@@ -139,25 +127,13 @@
 						<td class="p-3 text-sm whitespace-nowrap">{book.eventType}</td>
 						<td class="p-3 text-sm whitespace-nowrap">{book.date}</td>
 						<td class="p-3 text-sm whitespace-nowrap">{book.time}</td>
-						<td class="p-3 text-sm whitespace-nowrap">
-							<button on:click={approveBook(book.id)} type="button" class="py-2 px-2 text-green-500"
-								>Approve</button
-							>
-						</td>
-						<td class="p-3 text-sm whitespace-nowrap">
-							<button
-								on:click={disapproveBook(book.id)}
-								type="button"
-								class="py-2 px-2 text-red-500">Dissaprove</button
-							>
-						</td>
-						<td class="p-3 text-sm whitespace-nowrap">
-							<button
-								on:click={sendPaymentEmail(book.email)}
-								type="button"
-								class="py-2 px-2 text-blue-500">Send Payment</button
-							>
-						</td>
+						{#if book.status == 'Approved'}
+							<td class="p-3 text-sm whitespace-nowrap text-green-500">{book.status}</td>
+						{:else if book.status == 'Disapproved'}
+							<td class="p-3 text-sm whitespace-nowrap text-red-500">{book.status}</td>
+						{:else}
+							<td class="p-3 text-sm whitespace-nowrap">{book.status}</td>
+						{/if}
 					</tr>
 				{/each}
 			</tbody>
@@ -167,46 +143,35 @@
 	<!-- Small screen -->
 	<div class="bg-gray-300 my-5 p-5  selection:grid grid-cols-1 gap-4 md:hidden rounded-lg shadow">
 		{#each listOfBooking as book}
-			{#if book.status == 'Pending'}
-				<div class="bg-white space-y-3 p-4 border-2 border-black">
-					<div class="flex items-center space-x-2  text-sm">
-						<div>
-							<span class="font-bold text-sm">Name: </span>
-							{book.firstName + ' ' + book.lastName}
-						</div>
-					</div>
+			<div class="bg-white space-y-3 p-4 border-2 border-black">
+				<div class="flex items-center space-x-2  text-sm">
 					<div>
-						<span class="font-bold text-sm">E-mail Address: </span>
-						{book.email}
-					</div>
-					<div>
-						<span class="font-bold text-sm">Contact No: </span>
-						{book.contactNumber}
-					</div>
-					<div>
-						<span class="font-bold text-sm">Type of Event: </span>
-						{book.eventType}
-						<span class="font-bold text-sm">Date: </span>
-						{book.date}
-						<span class="font-bold text-sm">Time: </span>
-						{book.time}
-					</div>
-					<div>
-						<button on:click={approveBook(book.id)} type="button" class="py-2 px-2 text-green-500"
-							>Approve</button
-						>
-						<button on:click={disapproveBook(book.id)} type="button" class="py-2 px-2 text-red-500"
-							>Dissaprove</button
-						>
-						<button
-							on:click={sendPaymentEmail(book.email)}
-							type="button"
-							class="py-2 px-2 text-blue-500">Send Payment</button
-						>
+						<span class="font-bold text-sm">Name: </span>
+						{book.firstName + ' ' + book.lastName}
 					</div>
 				</div>
-				<br />
-			{/if}
+				<div>
+					<span class="font-bold text-sm">E-mail Address: </span>
+					{book.email}
+				</div>
+				<div>
+					<span class="font-bold text-sm">Contact No: </span>
+					{book.contactNumber}
+				</div>
+				<div>
+					<span class="font-bold text-sm">Type of Event: </span>
+					{book.eventType}
+				</div>
+				<div>
+					<span class="font-bold text-sm">Date: </span>
+					{book.date}
+					<span class="font-bold text-sm">Time: </span>
+					{book.time}
+					<span class="font-bold text-sm">Status: </span>
+					{book.status}
+				</div>
+			</div>
+			<br />
 		{/each}
 	</div>
 </div>
