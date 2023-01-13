61065bd151e3a2020ec0a8a1c5526b3fa28209cb
justinefer <justinefer@airmail.cc>
2023-01-13 15:33:56 +0800

    update popup component design

--- src/routes/(admin-side)/admin/calendar/Popup.svelte
+++ src/routes/(admin-side)/admin/calendar/Popup.svelte
@@ -1,29 +1,47 @@
 <script>
-    import { onSnapshot, query, collection, where } from 'firebase/firestore';
+	import { onSnapshot, query, collection, where } from 'firebase/firestore';
 	import { db } from '$lib/firebase/client';
-    import { calendarDate } from '$lib/store';
+	import { calendarDate } from '$lib/store';
 	import { onDestroy } from 'svelte';
 
-    let listOfEvents = [];
-    let eventsQuery = query(collection(db, 'event'), where('date', '==', $calendarDate));
+	const monthName = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	];
 
-    async function getEvents(eventsQuery) {
+	let listOfEvents = [];
+	let eventsQuery = query(collection(db, 'event'), where('date', '==', $calendarDate));
+	let dateTitle = $calendarDate.split('-');
+	let monthNumber = dateTitle[1];
+
+	async function getEvents(eventsQuery) {
 		const unsubscribe = onSnapshot(eventsQuery, (querySnapshot) => {
 			listOfEvents = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 		});
 		onDestroy(() => unsubscribe());
 	}
 
-    $: getEvents(eventsQuery);
+	$: getEvents(eventsQuery);
 </script>
 
-
-<div>
-    <h1 class="font-bold text-xl pb-4">Events for {$calendarDate}</h1>
-    {#each listOfEvents as event}
-    <div class="p-3">
-        <h2 class="font-bold">{event.titleDisplay}</h2>
-        <p class="p-2">{event.description}</p>
-    </div>
-    {/each}
+<div class="p-3">
+	<h1 class="font-bold text-2xl pb-8">Events for {monthName[monthNumber - 1] + ' ' + dateTitle[2] + ', ' + dateTitle[0]}</h1>
+	{#each listOfEvents as event}
+		<div class="py-5">
+			<div class="bg-blue-500 rounded-lg">
+				<h2 class="text-lg font-bold py-3 px-4">{event.titleDisplay}</h2>
+			</div>
+			<p class="pt-7 px-4">{event.description}</p>
+		</div>
+	{/each}
 </div>
--- src/routes/(client-side)/calendar/Popup.svelte
+++ src/routes/(client-side)/calendar/Popup.svelte
@@ -4,8 +4,25 @@
 	import { calendarDate } from '$lib/store';
 	import { onDestroy } from 'svelte';
 
+	const monthName = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	];
+
 	let listOfEvents = [];
 	let eventsQuery = query(collection(db, 'event'), where('date', '==', $calendarDate));
+	let dateTitle = $calendarDate.split('-');
+	let monthNumber = dateTitle[1];
 
 	async function getEvents(eventsQuery) {
 		const unsubscribe = onSnapshot(eventsQuery, (querySnapshot) => {
@@ -17,12 +34,14 @@
 	$: getEvents(eventsQuery);
 </script>
 
-<div>
-	<h1 class="font-bold text-xl pb-4">Events for {$calendarDate}</h1>
+<div class="p-3">
+	<h1 class="font-bold text-2xl pb-8">Events for {monthName[monthNumber - 1] + ' ' + dateTitle[2] + ', ' + dateTitle[0]}</h1>
 	{#each listOfEvents as event}
-		<div class="p-3">
-			<h2 class="font-bold">{event.titleDisplay}</h2>
-			<p class="p-2">{event.description}</p>
+		<div class="py-5">
+			<div class="bg-blue-500 rounded-lg">
+				<h2 class="text-lg font-bold py-3 px-4">{event.titleDisplay}</h2>
+			</div>
+			<p class="pt-7 px-4">{event.description}</p>
 		</div>
 	{/each}
 </div>
