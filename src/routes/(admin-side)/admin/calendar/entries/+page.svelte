580caedb6ff019d140a0debb8519d432f78e3967
Rodel Gerodias <rodelgerodias25@gmail.com>
2023-02-23 18:51:40 +0800

    added pagination in news and events page

--- src/routes/(admin-side)/admin/calendar/entries/+page.svelte
+++ src/routes/(admin-side)/admin/calendar/entries/+page.svelte
@@ -9,9 +9,18 @@
 	let searchByValue = '';
 	let eventQuery = query(collection(db, 'event'));
 
-	async function getEvents(eventQuery) {
+	let currentPage = 1;
+	let pageSize = 10;
+	let totalRecords = 1;
+	let totalPages = 0;
+
+	async function getEvents(eventQuery, page, pageSize) {
+		const startIndex = (page - 1) * pageSize;
+		const endIndex = startIndex + pageSize;
 		const unsubscribe = onSnapshot(eventQuery, (querySnapshot) => {
-			listOfEvents = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+			listOfEvents = querySnapshot.docs
+				.map((doc) => ({ id: doc.id, ...doc.data() }))
+				.slice(startIndex, endIndex);
 		});
 		onDestroy(() => unsubscribe());
 	}
@@ -34,7 +43,17 @@
 		searchByValue = '';
 	}
 
-	$: getEvents(eventQuery);
+	$: {
+		getEvents(eventQuery, currentPage, pageSize);
+		const unsubscribe = onSnapshot(eventQuery, (querySnapshot) => {
+			totalRecords = querySnapshot.docs.length;
+			totalPages = Math.ceil(totalRecords / pageSize);
+		});
+		onDestroy(() => unsubscribe());
+	}
+	function goToPage(page) {
+		currentPage = page;
+	}
 </script>
 
 <svelte:head>
@@ -114,11 +133,60 @@
 				{/each}
 			</tbody>
 		</table>
-	</div>
-	<div class="flex mx-auto items-center justify-center my-8">
-		<div class="grid grid-cols-2">
-			<button class="btn btn-primary mx-1">Previous</button>
-			<button class="btn btn-primary mx-1">Next</button>
+		<!-- pagination button -->
+		<div class="flex justify-center items-center mt-5">
+			<nav class="block">
+				<ul class="flex pl-0 rounded list-none flex-wrap">
+					{#if currentPage > 1}
+						<li>
+							<button
+								class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
+								on:click={() => goToPage(currentPage - 1)}
+							>
+								Previous
+							</button>
+						</li>
+					{/if}
+
+					{#each Array.from({ length: totalPages }, (_, i) => i + 1) as page}
+						{#if page === currentPage}
+							<li>
+								<button
+									class="relative block py-2 px-3 leading-tight bg-blue-700 text-white hover:bg-blue-500 focus:bg-blue-500"
+									>{page}</button
+								>
+							</li>
+						{:else if (page >= currentPage - 2 && page <= currentPage + 2) || page === totalPages || page === 1}
+							<li>
+								<button
+									class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
+									on:click={() => goToPage(page)}
+								>
+									{page}
+								</button>
+							</li>
+						{:else if page === currentPage - 3 || page === currentPage + 3}
+							<li>
+								<span
+									class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700"
+									>...</span
+								>
+							</li>
+						{/if}
+					{/each}
+
+					{#if currentPage < totalPages}
+						<li>
+							<button
+								class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
+								on:click={() => goToPage(currentPage + 1)}
+							>
+								Next
+							</button>
+						</li>
+					{/if}
+				</ul>
+			</nav>
 		</div>
 	</div>
 
