1324beb8ad8784341a169d80362a964705e4f472
Rodel Gerodias <rodelgerodias25@gmail.com>
2023-02-24 14:29:13 +0800

    move pagination button

--- src/routes/(admin-side)/admin/accounts/+page.svelte
+++ src/routes/(admin-side)/admin/accounts/+page.svelte
@@ -156,62 +156,6 @@
 					{/each}
 				</tbody>
 			</table>
-
-			<!-- pagination button -->
-			<div class="flex justify-center items-center mt-5">
-				<nav class="block">
-					<ul class="flex pl-0 rounded list-none flex-wrap">
-						{#if currentPage > 1}
-							<li>
-								<button
-									class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
-									on:click={() => goToPage(currentPage - 1)}
-								>
-									Previous
-								</button>
-							</li>
-						{/if}
-
-						{#each Array.from({ length: totalPages }, (_, i) => i + 1) as page}
-							{#if page === currentPage}
-								<li>
-									<button
-										class="relative block py-2 px-3 leading-tight bg-blue-700 text-white hover:bg-blue-500 focus:bg-blue-500"
-										>{page}</button
-									>
-								</li>
-							{:else if (page >= currentPage - 2 && page <= currentPage + 2) || page === totalPages || page === 1}
-								<li>
-									<button
-										class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
-										on:click={() => goToPage(page)}
-									>
-										{page}
-									</button>
-								</li>
-							{:else if page === currentPage - 3 || page === currentPage + 3}
-								<li>
-									<span
-										class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700"
-										>...</span
-									>
-								</li>
-							{/if}
-						{/each}
-
-						{#if currentPage < totalPages}
-							<li>
-								<button
-									class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
-									on:click={() => goToPage(currentPage + 1)}
-								>
-									Next
-								</button>
-							</li>
-						{/if}
-					</ul>
-				</nav>
-			</div>
 		</div>
 	</div>
 
@@ -252,6 +196,62 @@
 			</div>
 		{/each}
 	</div>
+</div>
+
+<!-- pagination button -->
+<div class="flex justify-center items-center mt-5">
+	<nav class="block">
+		<ul class="flex pl-0 rounded list-none flex-wrap">
+			{#if currentPage > 1}
+				<li>
+					<button
+						class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
+						on:click={() => goToPage(currentPage - 1)}
+					>
+						Previous
+					</button>
+				</li>
+			{/if}
+
+			{#each Array.from({ length: totalPages }, (_, i) => i + 1) as page}
+				{#if page === currentPage}
+					<li>
+						<button
+							class="relative block py-2 px-3 leading-tight bg-blue-700 text-white hover:bg-blue-500 focus:bg-blue-500"
+							>{page}</button
+						>
+					</li>
+				{:else if (page >= currentPage - 2 && page <= currentPage + 2) || page === totalPages || page === 1}
+					<li>
+						<button
+							class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
+							on:click={() => goToPage(page)}
+						>
+							{page}
+						</button>
+					</li>
+				{:else if page === currentPage - 3 || page === currentPage + 3}
+					<li>
+						<span
+							class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700"
+							>...</span
+						>
+					</li>
+				{/if}
+			{/each}
+
+			{#if currentPage < totalPages}
+				<li>
+					<button
+						class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
+						on:click={() => goToPage(currentPage + 1)}
+					>
+						Next
+					</button>
+				</li>
+			{/if}
+		</ul>
+	</nav>
 </div>
 
 <!-- mema comment -->
--- src/routes/(admin-side)/admin/calendar/entries/+page.svelte
+++ src/routes/(admin-side)/admin/calendar/entries/+page.svelte
@@ -117,7 +117,7 @@
 					<tr class="hover">
 						<td class="count" />
 						<td />
-						<td>{event.titleDisplay}</td>
+						<td>{event.titleDisplay.substring(0, 20) + '...'}</td>
 						<td>{event.description.substring(0, 50) + '...'}</td>
 						<td>{event.date}</td>
 						<td>
@@ -133,61 +133,6 @@
 				{/each}
 			</tbody>
 		</table>
-		<!-- pagination button -->
-		<div class="flex justify-center items-center mt-5">
-			<nav class="block">
-				<ul class="flex pl-0 rounded list-none flex-wrap">
-					{#if currentPage > 1}
-						<li>
-							<button
-								class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
-								on:click={() => goToPage(currentPage - 1)}
-							>
-								Previous
-							</button>
-						</li>
-					{/if}
-
-					{#each Array.from({ length: totalPages }, (_, i) => i + 1) as page}
-						{#if page === currentPage}
-							<li>
-								<button
-									class="relative block py-2 px-3 leading-tight bg-blue-700 text-white hover:bg-blue-500 focus:bg-blue-500"
-									>{page}</button
-								>
-							</li>
-						{:else if (page >= currentPage - 2 && page <= currentPage + 2) || page === totalPages || page === 1}
-							<li>
-								<button
-									class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
-									on:click={() => goToPage(page)}
-								>
-									{page}
-								</button>
-							</li>
-						{:else if page === currentPage - 3 || page === currentPage + 3}
-							<li>
-								<span
-									class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700"
-									>...</span
-								>
-							</li>
-						{/if}
-					{/each}
-
-					{#if currentPage < totalPages}
-						<li>
-							<button
-								class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
-								on:click={() => goToPage(currentPage + 1)}
-							>
-								Next
-							</button>
-						</li>
-					{/if}
-				</ul>
-			</nav>
-		</div>
 	</div>
 
 	<!-- Small screen -->
@@ -195,10 +140,10 @@
 		{#each listOfEvents as event}
 			<div class="card w-[105%] bg-base-100 shadow-xl">
 				<div class="card-body">
-					<h2 class="card-title mb-2">{event.titleDisplay}</h2>
+					<h2 class="card-title mb-2">{event.titleDisplay.substring(0, 20) + '...'}</h2>
 					<div>
 						<span class="my-1 font-bold">Description:</span>
-						{event.description}
+						{event.description.substring(0, 30) + '...'}
 					</div>
 					<div>
 						<span class="my-1 font-bold">Date:</span>
@@ -217,4 +162,60 @@
 			</div>
 		{/each}
 	</div>
+</div>
+
+<!-- pagination button -->
+<div class="flex justify-center items-center mt-5">
+	<nav class="block">
+		<ul class="flex pl-0 rounded list-none flex-wrap">
+			{#if currentPage > 1}
+				<li>
+					<button
+						class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
+						on:click={() => goToPage(currentPage - 1)}
+					>
+						Previous
+					</button>
+				</li>
+			{/if}
+
+			{#each Array.from({ length: totalPages }, (_, i) => i + 1) as page}
+				{#if page === currentPage}
+					<li>
+						<button
+							class="relative block py-2 px-3 leading-tight bg-blue-700 text-white hover:bg-blue-500 focus:bg-blue-500"
+							>{page}</button
+						>
+					</li>
+				{:else if (page >= currentPage - 2 && page <= currentPage + 2) || page === totalPages || page === 1}
+					<li>
+						<button
+							class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
+							on:click={() => goToPage(page)}
+						>
+							{page}
+						</button>
+					</li>
+				{:else if page === currentPage - 3 || page === currentPage + 3}
+					<li>
+						<span
+							class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700"
+							>...</span
+						>
+					</li>
+				{/if}
+			{/each}
+
+			{#if currentPage < totalPages}
+				<li>
+					<button
+						class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
+						on:click={() => goToPage(currentPage + 1)}
+					>
+						Next
+					</button>
+				</li>
+			{/if}
+		</ul>
+	</nav>
 </div>
