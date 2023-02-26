42a76e026969d8b2bd553c7eaa200c7c2111e57d
justine <justinefer@airmail.cc>
2023-02-26 15:58:25 +0800

    fix paymentMethod source

--- src/routes/api/paymongo/paid-webhook/+server.js
+++ src/routes/api/paymongo/paid-webhook/+server.js
@@ -11,7 +11,7 @@ export async function POST({request}) {
     const body = await request.json()
     await addDoc(collection(db, 'paymongo'), body)
     const paymentDesc = body.data.attributes.data.attributes.description
-    const paymentMethod = body.data.attributes.data.attributes.attributes.type
+    const paymentMethod = body.data.attributes.data.attributes.data.attributes.source.type
     const bookID = body.data.attributes.data.attributes.remarks
     if (paymentDesc == 'Clubhouse Reservation Downpayment') {
         await updateDoc(doc(db, 'booking', bookID), {paymentStatus: 'Paid'})
