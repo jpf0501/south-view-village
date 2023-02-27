08e36d8af100fc23f1f456102815b82cc9c92e8d
Rodel Gerodias <rodelgerodias25@gmail.com>
2023-02-27 17:03:51 +0800

    redirection function to admin only if admin

--- src/routes/(client-side)/+layout.svelte
+++ src/routes/(client-side)/+layout.svelte
@@ -1,14 +1,41 @@
 <script>
-    import Footer from './Footer.svelte';
+	import Footer from './Footer.svelte';
 	import Header from './Header.svelte';
 	import Nav from './Nav.svelte';
+	import { userStore } from '$lib/store';
+	import { getDoc, doc } from 'firebase/firestore';
+	import { db } from '$lib/firebase/client';
+	import { goto } from '$app/navigation';
+
+	let show = false;
+	async function checkIfAdmin(id) {
+		const snapshot = await getDoc(doc(db, 'accounts', id));
+		const role = snapshot.get('role');
+		const isAdmin = 'Admin' === role;
+		if (!isAdmin) {
+			await goto('/');
+		} else {
+			await goto('/admin');
+			show = true;
+		}
+	}
+
+	$: {
+		if ($userStore === null) {
+			goto('/');
+		} else if ($userStore) {
+			checkIfAdmin($userStore.uid);
+		}
+	}
 </script>
 
-
-<main class="flex flex-col min-h-full">
-	<Header/>
-	<Nav/>
-	<slot/>
-	<Footer/>
-</main>
-
+{#if !show}
+	Loading
+{:else}
+	<main class="flex flex-col min-h-full">
+		<Header />
+		<Nav />
+		<slot />
+		<Footer />
+	</main>
+{/if}
