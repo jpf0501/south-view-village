f1231b10e26c246a93616adee69f7b05dfd4f6dd
justine <justinefer@airmail.cc>
2023-03-05 03:17:44 +0800

    date tooltip

--- src/routes/(client-side)/news/entry/[newsID]/+page.svelte
+++ src/routes/(client-side)/news/entry/[newsID]/+page.svelte
@@ -54,7 +54,8 @@
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <p>{news.dateCreated.toDate().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric'})} by Admin | Last Updated {news.dateModified.toDate().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric'})}</p>
+            <p><span class="tooltip" data-tip='{news.dateCreated.toDate().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })} {news.dateCreated.toDate().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}'>{news.dateCreated.toDate().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric'})}</span> by Admin | Last Updated <span class="tooltip" data-tip='{news.dateModified.toDate().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric'})} {news.dateModified.toDate().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}'>{news.dateModified.toDate().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric'})}</span></p>
+
         </div>
         <h1 class="divider">           
         </h1>
