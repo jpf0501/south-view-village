502acdc5ed3d8eb8ac23b2ffa17e5c667e69adf5
Rodel Gerodias <rodelgerodias25@gmail.com>
2023-03-04 21:55:56 +0800

    add an pagination component

--- src/routes/(admin-side)/admin/calendar/entries/+page.svelte
+++ src/routes/(admin-side)/admin/calendar/entries/+page.svelte
@@ -9,6 +9,7 @@
 	} from 'firebase/firestore';
 	import { db } from '$lib/firebase/client';
 	import { onDestroy } from 'svelte';
+	import Pagination from '../../Pagination.svelte';
 
 	let listOfEvents = [];
 	let sortByField = '';
@@ -185,57 +186,4 @@
 </div>
 
 <!-- pagination button -->
-<div class="flex justify-center items-center mt-5">
-	<nav class="block">
-		<ul class="flex pl-0 rounded list-none flex-wrap">
-			{#if currentPage > 1}
-				<li>
-					<button
-						class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
-						on:click={() => goToPage(currentPage - 1)}
-					>
-						Previous
-					</button>
-				</li>
-			{/if}
-
-			{#each Array.from({ length: totalPages }, (_, i) => i + 1) as page}
-				{#if page === currentPage}
-					<li>
-						<button
-							class="relative block py-2 px-3 leading-tight bg-blue-700 text-white hover:bg-blue-500 focus:bg-blue-500"
-							>{page}</button
-						>
-					</li>
-				{:else if (page >= currentPage - 2 && page <= currentPage + 2) || page === totalPages || page === 1}
-					<li>
-						<button
-							class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
-							on:click={() => goToPage(page)}
-						>
-							{page}
-						</button>
-					</li>
-				{:else if page === currentPage - 3 || page === currentPage + 3}
-					<li>
-						<span
-							class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700"
-							>...</span
-						>
-					</li>
-				{/if}
-			{/each}
-
-			{#if currentPage < totalPages}
-				<li>
-					<button
-						class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 hover:bg-gray-200 focus:bg-gray-200"
-						on:click={() => goToPage(currentPage + 1)}
-					>
-						Next
-					</button>
-				</li>
-			{/if}
-		</ul>
-	</nav>
-</div>
+<Pagination {currentPage} {totalPages} onPageChange={goToPage} />
