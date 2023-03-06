15e3446d4b92f0f0c9e87b17eec6bfd23bed0c80
justine <justinefer@airmail.cc>
2023-03-06 18:21:04 +0800

    update footer

--- src/routes/(client-side)/Footer.svelte
+++ src/routes/(client-side)/Footer.svelte
@@ -1,4 +1,9 @@
-<footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
+<script>
+	let d = new Date()
+	let year = d.getFullYear();
+</script>
+
+<!-- <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
 	<div class="flex flex-row">
 		<img class="mask mask-circle" src="/logo.png" alt="Southview Homes 3 Logo"/>
 		<p class="text-lg">Southview Homes 3</p>
@@ -21,4 +26,46 @@
 	<div class="mt-2">
 		<p>Copyright © 2022 Southview Homes 3 - All Rights Reserved</p>
 	</div>
+</footer> -->
+
+<footer class="footer p-10 bg-base-200 text-base-content">
+  <div class="pl-24 py-8">
+    <img class="mask mask-circle" src="/logo.png" alt="Southview Homes 3 Logo"/>
+    <p class="pt-3 font-bold">Southview Homes 3</p>
+	<p>Copyright © {year} Southview Homes 3 - All Rights Reserved</p>
+  </div> 
+  <div class="py-5">
+	<span class="footer-title">Quick Links</span>
+    <a href="/calendar" class="link link-hover">Calendar</a> 
+    <a href="/committee" class="link link-hover">News</a> 
+    <a href="/news" class="link link-hover">Committee</a> 
+    <a href="/reservation" class="link link-hover">Reservation</a>
+  </div> 
+  <div class="py-5">
+	<span class="footer-title">Legal</span>
+    <a href="/terms-of-use" class="link link-hover">Terms of Use</a> 
+    <a href="/privacy-policy" class="link link-hover">Privacy Policy</a>
+  </div> 
+  <div class="py-5">
+    <span class="footer-title pb-2">Contact</span> 
+	<div class="flex items-center">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+			<path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+			<path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+		</svg>		  
+		<p class="pl-4">Southview Homes 3, San Vicente Road,<br/>Brgy. San Vicente , City of San Pedro,Laguna</p> 
+	</div>
+	<div class="flex items-center py-2">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+			<path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
+		</svg>		  
+		<p class="pl-4">8330-4163 / 09063955407</p> 
+	</div>
+	<div class="flex items-center">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+			<path fill-rule="evenodd" d="M17.834 6.166a8.25 8.25 0 100 11.668.75.75 0 011.06 1.06c-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788 3.807-3.808 9.98-3.808 13.788 0A9.722 9.722 0 0121.75 12c0 .975-.296 1.887-.809 2.571-.514.685-1.28 1.179-2.191 1.179-.904 0-1.666-.487-2.18-1.164a5.25 5.25 0 11-.82-6.26V8.25a.75.75 0 011.5 0V12c0 .682.208 1.27.509 1.671.3.401.659.579.991.579.332 0 .69-.178.991-.579.3-.4.509-.99.509-1.671a8.222 8.222 0 00-2.416-5.834zM15.75 12a3.75 3.75 0 10-7.5 0 3.75 3.75 0 007.5 0z" clip-rule="evenodd" />
+		</svg>
+		<p class="pl-4">southviewhomes3mail@gmail.com</p> 
+	</div>
+  </div>
 </footer>
