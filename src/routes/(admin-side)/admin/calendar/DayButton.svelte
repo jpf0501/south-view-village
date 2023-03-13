5a392cfa915d9ef7517709df27569900fcb66fc2
Rodel Gerodias <rodelgerodias25@gmail.com>
2023-03-13 20:27:37 +0800

    update calendar in admin side

--- src/routes/(admin-side)/admin/calendar/DayButton.svelte
+++ src/routes/(admin-side)/admin/calendar/DayButton.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { getDocs, collection, query, where } from 'firebase/firestore';
 	import { db } from '$lib/firebase/client';
-	import { calendarDate } from '$lib/store'
+	import { calendarDate } from '$lib/store';
 	import { getContext } from 'svelte';
 	import Popup from './Popup.svelte';
 
@@ -10,30 +10,58 @@
 	export let year;
 
 	$: dateString = new Date(`${year}-${month}-${dayNumber}`).toLocaleDateString('fr-CA');
-	
+	$: newDateString = new Date(`${year}-${month}-${dayNumber}`);
+
 	let isThereEvent = false;
+	let isThereBooking = false;
 	const { open } = getContext('simple-modal');
-	// const showModal = () => open(Popup);
 
 	async function checkIfExistingEvent(dateString) {
-		const querySnapshot = await getDocs(
+		const querySnapshotE = await getDocs(
 			query(collection(db, 'event'), where('date', '==', dateString))
 		);
-		isThereEvent = !querySnapshot.empty;
+		isThereEvent = !querySnapshotE.empty;
+	}
+
+	async function checkIfExistingBook(newDateString) {
+		const lastDayOfMonth = new Date(year, month, 0).getDate();
+		let afterDateString = new Date(`${year}-${month}-${Math.min(dayNumber + 1, lastDayOfMonth)}`);
+		if (dayNumber === lastDayOfMonth) {
+			// if next day is the first day of the next month
+			afterDateString = new Date(`${year}-${month + 1}-01`);
+		}
+		if (month === 12) {
+			afterDateString = new Date(`${year + 1}-01-01`);
+		}
+		const querySnapshotB = await getDocs(
+			query(
+				collection(db, 'booking'),
+				where('status', '==', 'Approved'),
+				where('bookDate', '>=', newDateString),
+				where('bookDate', '<', afterDateString)
+			)
+		);
+		isThereBooking = !querySnapshotB.empty;
 	}
 
 	export function showModal() {
-		calendarDate.set(dateString)
+		calendarDate.set(dateString);
 		open(Popup);
 	}
-	
+
 	$: checkIfExistingEvent(dateString);
+	$: checkIfExistingBook(newDateString);
 </script>
 
 <button
-    class="border border-collapse aspect-video {isThereEvent ? 'bg-blue-700 text-white' : ''}"
-    on:click={showModal}
-    disabled={!isThereEvent}
+	class="border border-collapse aspect-video {isThereEvent && isThereBooking
+		? 'bg-red-500'
+		: ''}{isThereBooking && !isThereEvent ? 'bg-blue-700 text-white' : ''} {isThereEvent &&
+	!isThereBooking
+		? 'bg-green-500'
+		: ''}"
+	on:click={showModal}
+	disabled={!isThereBooking && !isThereEvent}
 >
 	{dayNumber}
 </button>
--- src/routes/(admin-side)/admin/calendar/Popup.svelte
+++ src/routes/(admin-side)/admin/calendar/Popup.svelte
@@ -1,8 +1,7 @@
 <script>
-	import { onSnapshot, query, collection, where } from 'firebase/firestore';
+	import { getDocs, query, collection, where } from 'firebase/firestore';
 	import { db } from '$lib/firebase/client';
 	import { calendarDate } from '$lib/store';
-	import { onDestroy } from 'svelte';
 
 	const monthName = [
 		'January',
@@ -19,29 +18,106 @@
 		'December'
 	];
 
-	let listOfEvents = [];
-	let eventsQuery = query(collection(db, 'event'), where('date', '==', $calendarDate));
+	// convert $calendarDate to a Date object
+	let currentDayTimeStamp = new Date($calendarDate);
+
+	// Convert $calendarDate to a Date object
+	// Para makuha date object ng next day
+	let nextDayTimeStamp = new Date($calendarDate);
+
+	// Get the year and month of $calendarDate
+	let year = nextDayTimeStamp.getFullYear();
+	let month = nextDayTimeStamp.getMonth();
+
+	// Get the last day of the month
+	let lastDayOfMonth = new Date(year, month + 1, 0).getDate();
+
+	// Check if $calendarDate is the last day of the month
+	if (nextDayTimeStamp.getDate() === lastDayOfMonth) {
+		// If $calendarDate is the last day of the month, increment the month and set the day to 1
+		month++;
+		nextDayTimeStamp.setMonth(month);
+		nextDayTimeStamp.setDate(1);
+
+		// Check if $calendarDate is the last day of the year
+		if (month === 12) {
+			year++;
+			nextDayTimeStamp.setFullYear(year);
+		}
+	} else {
+		// If $calendarDate is not the last day of the month, simply increment the day by 1
+		nextDayTimeStamp.setDate(nextDayTimeStamp.getDate() + 1);
+	}
+
+	// Format the next day as yyyy-mm-dd
+	console.log(nextDayTimeStamp);
+
 	let dateTitle = $calendarDate.split('-');
 	let monthNumber = dateTitle[1];
 
-	async function getEvents(eventsQuery) {
-		const unsubscribe = onSnapshot(eventsQuery, (querySnapshot) => {
-			listOfEvents = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-		});
-		onDestroy(() => unsubscribe());
+	let listOfBookings = [];
+	let listOfEvents = [];
+	let eventsQuery = query(collection(db, 'event'), where('date', '==', $calendarDate));
+	let booksQuery = query(
+		collection(db, 'booking'),
+		where('status', '==', 'Approved'),
+		where('bookDate', '>=', currentDayTimeStamp),
+		where('bookDate', '<', nextDayTimeStamp)
+	);
+
+	let isThereEvent = false;
+	let isThereBooking = false;
+
+	async function getEventsAndBookings() {
+		const booksSnapshot = await getDocs(booksQuery);
+		listOfBookings = booksSnapshot.docs.map((doc) => doc.data());
+
+		const eventsSnapshot = await getDocs(eventsQuery);
+		listOfEvents = eventsSnapshot.docs.map((doc) => doc.data());
+
+		!booksSnapshot.empty ? (isThereBooking = true) : (isThereBooking = false);
+		!eventsSnapshot.empty ? (isThereEvent = true) : (isThereEvent = false);
 	}
-	onDestroy(() => unsubscribe());
-	$: getEvents(eventsQuery);
+
+	getEventsAndBookings();
 </script>
 
 <div class="p-3">
-	<h1 class="font-bold text-2xl pb-8">Events for {monthName[monthNumber - 1] + ' ' + dateTitle[2] + ', ' + dateTitle[0]}</h1>
-	{#each listOfEvents as event}
-		<div class="py-5">
-			<div class="bg-blue-500 rounded-lg">
-				<h2 class="text-lg font-bold py-3 px-4">{event.titleDisplay}</h2>
+	{#if isThereBooking}
+		<h1 class="font-bold text-2xl pb-8">
+			Bookings for {monthName[monthNumber - 1] + ' ' + dateTitle[2] + ', ' + dateTitle[0]}
+		</h1>
+		{#each listOfBookings as book}
+			<div class="py-5">
+				<div class="bg-blue-500 rounded-lg">
+					<h2 class="text-lg font-bold py-3 px-4">{book.eventTypeDisplay}</h2>
+				</div>
+				<p class="pt-7 px-4">
+					{book.bookDate.toDate().toLocaleDateString('en-us', {
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric'
+					}) +
+						' at ' +
+						book.bookDate
+							.toDate()
+							.toLocaleTimeString('en-us', { hour: '2-digit', minute: '2-digit' })}
+				</p>
 			</div>
-			<p class="pt-7 px-4">{event.description}</p>
-		</div>
-	{/each}
+		{/each}
+	{/if}
+
+	{#if isThereEvent}
+		<h1 class="font-bold text-2xl pb-8">
+			Events for {monthName[monthNumber - 1] + ' ' + dateTitle[2] + ', ' + dateTitle[0]}
+		</h1>
+		{#each listOfEvents as event}
+			<div class="py-5">
+				<div class="bg-blue-500 rounded-lg">
+					<h2 class="text-lg font-bold py-3 px-4">{event.titleDisplay}</h2>
+				</div>
+				<p class="pt-7 px-4">{event.description}</p>
+			</div>
+		{/each}
+	{/if}
 </div>
