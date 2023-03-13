675a17a4e681b4d6fec2079efa726fc7f17343dc
Rodel Gerodias <rodelgerodias25@gmail.com>
2023-03-13 21:31:15 +0800

    added form validation in edit events

--- src/routes/(admin-side)/admin/calendar/entries/edit/[eventID]/+page.svelte
+++ src/routes/(admin-side)/admin/calendar/entries/edit/[eventID]/+page.svelte
@@ -1,23 +1,45 @@
 <script>
-    import { db } from '$lib/firebase/client';
+	import { db } from '$lib/firebase/client';
 	import { getDoc, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 	import { goto } from '$app/navigation';
 	import toast from 'svelte-french-toast';
 
-    /** @type {import('./$types').PageData} */
+	/** @type {import('./$types').PageData} */
 	export let data;
 	const { eventID } = data;
 
-    let event = null;
+	let event = null;
+	let errors = {};
 
-    async function getEvent() {
+	async function getEvent() {
 		const snapshot = await getDoc(doc(db, 'event', eventID));
 		event = snapshot.data();
-        event.title = event.titleDisplay;
+		event.title = event.titleDisplay;
 	}
 
+	async function checkInput() {
+		event.title = event.titleDisplay.toLowerCase();
+		errors = {
+			title: !event.title,
+			description: !event.description,
+			date: !event.date,
+			descriptionKulang: event.description.length < 11
+		};
+		if (Object.values(errors).some((v) => v)) {
+			setTimeout(() => {
+				errors = {};
+			}, 2000);
+			return;
+		}
+		return true;
+	}
 
 	async function updateEvent() {
+		const isValid = await checkInput();
+		if (!isValid) {
+			toast.error('Form validation failed');
+			return;
+		}
 		event.title = event.titleDisplay.toLowerCase();
 		try {
 			await updateDoc(doc(db, 'event', eventID), event);
@@ -41,7 +63,6 @@
 	}
 
 	getEvent();
-
 </script>
 
 <svelte:head>
@@ -49,9 +70,9 @@
 </svelte:head>
 
 {#if event}
-<div class="min-h-screen hero bg-base-200">
-	<div class="w-full max-w-4xl p-6 mx-auto shadow-2xl border rounded-xl bg-base-100">
-		<h1 class="text-2xl mt-2">Edit Event</h1>
+	<div class="min-h-screen hero bg-base-200">
+		<div class="w-full max-w-4xl p-6 mx-auto shadow-2xl border rounded-xl bg-base-100">
+			<h1 class="text-2xl mt-2">Edit Event</h1>
 			<div class="grid grid-cols-1 gap-6 mt-6 md:grid-cols-2">
 				<div class="form-control">
 					<span class="pb-3">Event Title</span>
@@ -86,8 +107,10 @@
 			<div class="flex justify-end mt-8">
 				<button type="submit" on:click={updateEvent} class="btn btn-primary">Save</button>
 				<a href="/admin/calendar/entries/" class="btn btn-error mx-1 text-white">Cancel</a>
-                <button type="submit" on:click={deleteEvent} class="btn btn-warning text-white">Delete</button>
+				<button type="submit" on:click={deleteEvent} class="btn btn-warning text-white"
+					>Delete</button
+				>
 			</div>
+		</div>
 	</div>
-</div>
 {/if}
