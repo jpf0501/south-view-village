f311f44f6e502a270112db550475c09950b59fa9
Rodel Gerodias <rodelgerodias25@gmail.com>
2023-03-14 17:31:07 +0800

    fix bug in search complaints

--- src/routes/(admin-side)/admin/complaint/+page.svelte
+++ src/routes/(admin-side)/admin/complaint/+page.svelte
@@ -28,6 +28,7 @@
 			where(searchByField, '>=', searchByValueCase),
 			where(searchByField, '<=', searchByValueCase + '~'),
 			where('hadAnswered', '==', false),
+			orderBy(searchByField, 'asc'),
 			orderBy('dateSubmitted', 'asc')
 		);
 	}
