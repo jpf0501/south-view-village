5547389dc34b0c8f38b2b1ac1af2ae9ea1993247
Rodel Gerodias <rodelgerodias25@gmail.com>
2023-07-12 10:38:02 +0800

    create form components

---
+++ src/lib/FormComponents/ContactForm.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let value;
+    export let isDisabled = false
+  </script>
+
+<div class="form-control">
+    <label for="lastname" class="label">
+        <span class="label-text">Contact no:</span>
+    </label>
+    <input
+        type="tel"
+        onkeypress="return event.charCode >= 48 && event.charCode <= 57"
+        minlength="11"
+        maxlength="11"
+        placeholder="09123456789"
+        pattern={String.raw`^(09)\d{9}$`}
+        name="contact"
+        class="input input-bordered"
+        bind:value={value}
+        disabled={isDisabled}
+     required
+    />
+</div>
---
+++ src/lib/FormComponents/LastnameForm.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let value;
+    export let isDisabled = false
+  </script>
+  
+  <div class="form-control">
+    <label for="lastname" class="label">
+      <span class="label-text">Last Name</span>
+    </label>
+    <input
+      type="text"
+      placeholder="Dela Cruz"
+      name="lname"
+      class="input input-bordered"
+      bind:value={value}
+      disabled={isDisabled}
+    required
+    />
+  </div>
+  
