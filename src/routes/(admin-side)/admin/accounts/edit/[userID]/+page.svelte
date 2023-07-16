82848ce0d88084e5b5c79106a4d6a6a9d487309d
Justine Fernando <jpfernando@sscgi.com>
2023-07-16 22:48:39 +0800

    middle name field for accounts

--- src/routes/(admin-side)/admin/accounts/edit/[userID]/+page.svelte
+++ src/routes/(admin-side)/admin/accounts/edit/[userID]/+page.svelte
@@ -12,6 +12,8 @@
 	let user = null;
 	let streetQuery = query(collection(db, 'street'), orderBy('streetName', 'asc'));
 	let listOfStreets = [];
+	let middleInitial = false;
+	let middlePrevious = '';
 
 	let errors = {};
 
@@ -28,6 +30,9 @@
 		user = snapshot.data();
 		user.firstname = user.firstNameDisplay;
 		user.lastname = user.lastNameDisplay;
+		user.middlename = user.middleNameDisplay;
+		middleInitial = (user.middlename == ' ') ? true : false
+		console.log(user.middlename)
 	}
 
 	async function getStreet() {
@@ -36,13 +41,16 @@
 	}
 
 	async function checkInput() {
-		const regex = /^[a-zA-Z -]*$/;
+		const regex = /^[a-zA-Z -\u00f1\u00d1]*$/;
+		const middleHash = /^[a-zA-Z \u00f1\u00d1]*$/;
 		errors = {
 			firstname: !user.firstNameDisplay,
 			lastname: !user.lastNameDisplay,
+			middlename: middleInitial ? user.middleNameDisplay == '' : !user.middleNameDisplay,
 			contactNumber: !user.contactNumber,
 			invalidFirstname: !regex.test(user.firstNameDisplay),
-			invalidLastname: !regex.test(user.lastNameDisplay)
+			invalidLastname: !regex.test(user.lastNameDisplay),
+			invalidMiddlename: !middleHash.test(user.middleNameDisplay)
 		};
 		if (Object.values(errors).some((v) => v)) {
 			setTimeout(() => {
@@ -63,6 +71,11 @@
 		}
 		user.firstname = user.firstNameDisplay.trim().toLowerCase();
 		user.lastname = user.lastNameDisplay.trim().toLowerCase();
+		user.middlename = user.middleNameDisplay.trim().toLowerCase();
+		user.firstNameDisplay = user.firstNameDisplay.trim()
+		user.lastNameDisplay = user.lastNameDisplay.trim()
+		user.middleNameDisplay = user.middleNameDisplay.trim()
+
 		try {
 			await updateDoc(doc(db, 'accounts', userID), user);
 			/*await addDoc(collection(db, 'adminlogs'), {
@@ -89,6 +102,18 @@
 		}
 	}
 
+	async function middleHandler() {
+		if (!middleInitial) {
+			middleInitial = true
+			middlePrevious = user.middleNameDisplay;
+			user.middleNameDisplay = ' ';
+		} else {
+			middleInitial = false
+			middlePrevious = user.middleNameDisplay;
+			user.middleNameDisplay = user.middlename;
+		}
+	}
+
 	getUser();
 	getStreet(streetQuery);
 </script>
@@ -103,6 +128,22 @@
 			<div class="w-full max-w-4xl p-6 mx-auto shadow-2xl border rounded-xl bg-base-100">
 				<h1 class="text-2xl mt-2">Edit Account</h1>
 				<div class="grid grid-cols-2 gap-6 mt-6 md:grid-cols-3">
+					<div class="form-control">
+						<label for="lname" class="label">
+							<span class="label-text">Last Name</span>
+						</label>
+						{#if errors.lastname}
+							<p class="text-red-500 text-sm italic mb-1">Last Name is required</p>
+						{:else if errors.invalidLastname}
+							<p class="text-red-500 text-sm italic mb-1">Only letters and '-'</p>
+						{/if}
+						<input
+							type="text"
+							name="lname"
+							class="input input-bordered"
+							bind:value={user.lastNameDisplay}
+						/>
+					</div>
 					<div class="form-control">
 						<label for="fname" class="label">
 							<span class="label-text">First Name</span>
@@ -120,20 +161,35 @@
 						/>
 					</div>
 					<div class="form-control">
-						<label for="lname" class="label">
-							<span class="label-text">Last Name</span>
+						<label for="mname" class="label">
+							<span class="label-text">Middle Name</span>
 						</label>
-						{#if errors.lastname}
-							<p class="text-red-500 text-sm italic mb-1">Last Name is required</p>
-						{:else if errors.invalidLastname}
+						{#if errors.middlename}
+							<p class="text-red-500 text-sm italic mb-1">Middle Name is required</p>
+						{:else if errors.invalidMiddlename}
 							<p class="text-red-500 text-sm italic mb-1">Only letters and '-'</p>
 						{/if}
+						{#if middleInitial == false}
 						<input
 							type="text"
+							placeholder="Santos"
 							name="lname"
 							class="input input-bordered"
-							bind:value={user.lastNameDisplay}
+							bind:value={user.middleNameDisplay}
 						/>
+						{:else}
+						<input
+							type="text"
+							placeholder="Santos"
+							name="lname"
+							class="input input-bordered"
+							disabled
+						/>
+						{/if}
+						<div class="flex items-center mt-5">
+							<input id="checkbox" checked={middleInitial} on:click={middleHandler} type="checkbox" class="checkbox checkbox-primary">
+							<label for="checkbox" class="ml-2 text-sm font-medium">No middle name</label>
+						</div>
 					</div>
 				</div>
 				<div class="grid grid-cols-2 gap-6 mt-4 md:grid-cols-5">
